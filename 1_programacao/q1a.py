b1ee5a36de2d0215ea1faed1a105fb2bfec1ae45
Murilo <murilopaulino@live.com>
2021-05-29 22:30:22 -0300

    resolução do primeiro exercício

---
+++ 1_programacao/q1a.py
@@ -0,0 +1,17 @@
+#em python, tudo eh funcao. Assim sendo, faz-se necessario o uso de parenteses
+#concatenacao -> "," ou "+"
+# "=" -> recebe
+#nomes das funções iguais aos nomes fornecidos nos exemplos
+
+#Seu programa deve receber um nome e uma idade como input, printando as mensagem
+#“Qual e o nome? ” e “Qual e a idade? ”. Depois ele deve printar “A idade de x e y anos”
+#na tela.
+
+#nome -> variavel   //  idade -> variavel  //  input -> comando para receber um dado pelo(a) usuario(a)
+
+#solitacao do nome
+nome = input("Qual e o nome? ")
+#solicitacao da idade
+idade = input("Qual e a idade? ")
+#concatenacao exibicao dos dados que entraram
+print("A idade de", nome, "e", idade, "anos")
